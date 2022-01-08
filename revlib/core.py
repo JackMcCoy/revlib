372377286f8a269b486b2eea3b460b585103db52
JackMcCoy <fdrdavegill@gmail.com>
2022-01-07 23:42:16 -0500

    debugging

--- revlib/core.py
+++ revlib/core.py
@@ -369,8 +369,6 @@ class ReversibleSequential(torch.nn.Module):
         inp0, inp1 = inp.chunk(2, self.split_dim)
         zeros = torch.zeros_like(inp0)
         x0,x1,z0,z1,a = self.stem((inp0, inp1, zeros, zeros,*args))
-        print('ReversibleSequence Forward')
-        print(len(x))
-        [print(type(i)) for i in x[0]]
+
         x = torch.cat(self.replace_grad(x0[0],x1,z0,z1), dim=self.split_dim)
         return x, a
